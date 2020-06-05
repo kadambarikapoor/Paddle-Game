8269141aa206570b26aa20f137561f7a19974034
kadambarikapoor <66360446+kadambarikapoor@users.noreply.github.com>
2020-06-05 15:14:06 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,43 @@
+var ball,img,paddle;
+function preload() {
+  ballimg = loadImage('ball.png');
+  paddleimg=loadImage("paddle.png")
+}
+
+function setup() {
+  createCanvas(400, 400);
+  ball=createSprite(60,200,20,20);
+  ball.addImage (ballimg); 
+  ball.velocityX=0;  
+  paddle=createSprite(350,200,20,100);
+  paddle.addImage(paddleimg)
+}
+
+function draw() {
+  background(205,153,0);
+  edges = createEdgeSprites();
+  
+  ball.bounceOff (edges[0]);
+  ball.bounceOff (edges[2]);
+  ball.bounceOff (edges[3]);
+  
+  ball.bounceOff(paddle);
+  
+  paddle.collide(edges);
+  
+  
+  if(keyDown(UP_ARROW)){
+    paddle.velocityY = -5
+  }
+  
+  if(keyDown(DOWN_ARROW)) {
+    paddle.velocityY = 5
+  }
+  drawSprites();
+  
+}
+
+function keyPressed(space) {
+  ball.velocityX = random(1, 10);
+  ball.velocityY = random(1, 10); 
+}
